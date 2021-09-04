8074608ba03c522d95fc8428cdef97b9a2f11123
Mohammad Mohammad hemanimani <mohammadhemani@gmail.com>
2021-09-05 01:55:52 +0200

    Initial Commit

---
+++ Shared/SIgnUpDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIgnUpDemoApp.swift
+//  Shared
+//
+//  Created by user205179 on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SIgnUpDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
